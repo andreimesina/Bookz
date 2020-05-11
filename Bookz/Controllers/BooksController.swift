db1be9992da6ed30af416d294cc37bf669e0c139
Andrei Mesina <andreimesina98@gmail.com>
2020-05-11 04:04:34 +0300

    Added some BooksManager test calls inside BooksController

--- Bookz/Controllers/BooksController.swift
+++ Bookz/Controllers/BooksController.swift
@@ -8,7 +8,43 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class BooksController: UIViewController {
+    
+    let db = Firestore.firestore()
+    let booksManager = BooksManager()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        db.collection("books").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+        
+        booksManager.getBookLocalURL(fileName: "the_art_of_war.pdf") { url in
+            if let url = url {
+                print("BooksController successfully fetched book! Local URL \(url)")
+            } else {
+                print("BooksController failed to fetch the book!")
+            }
+        }
+        
+        booksManager.getAllBooks { books in
+            guard books != nil else {
+                return
+            }
+            
+            books?.forEach { book in
+                print(book)
+            }
+        }
+    }
     
 }

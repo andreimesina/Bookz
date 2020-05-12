3bd5647cf47992e297456ed0d69bd0ad2393bc1c
Andrei Mesina <andreimesina98@gmail.com>
2020-05-13 02:04:07 +0300

    Added GenresManager

--- Bookz/Managers/GenresManager.swift
+++ Bookz/Managers/GenresManager.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+/**
+ This component is responsible for handling
+ the data layer corresponding to the [Genre] model.
+*/
+class GenresManager {
+    
+    let firestore = Firestore.firestore()
+    
+    /**
+     Fetches an [Array] containing all the [Genre]s data.
+     */
+    func getAllGenres(completion: @escaping (Array<Genre>?) -> Void) {
+        var genres = Array<Genre>()
+        
+        firestore.collection("genres")
+            .getDocuments() { querySnapshot, error in
+                if let error = error {
+                    print("Failed to fetch genres: \(error)")
+                    completion(nil)
+                } else if let querySnapshot = querySnapshot {
+                    for document in querySnapshot.documents {
+                        let result = Result {
+                            try document.data(as: Genre.self)
+                        }
+                        
+                        switch result {
+                            case .success(let genre):
+                                if let genre = genre {
+                                    genres.append(genre)
+                                } else {
+                                    print("Genre does not exist.")
+                                }
+                            case .failure(let error):
+                                print("Error decoding genre: \(error)")
+                        }
+                    }
+                    completion(genres)
+                }
+            }
+    }
+    
+}

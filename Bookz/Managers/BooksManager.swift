7ea82ac56e07efdaffb6d68b88eb0eff25dd0d8e
Andrei Mesina <andreimesina98@gmail.com>
2020-05-13 02:03:44 +0300

    Added book cover image fetching method

--- Bookz/Managers/BooksManager.swift
+++ Bookz/Managers/BooksManager.swift
@@ -23,6 +23,7 @@ class BooksManager {
     
     let baseLocalURL = DirectoryUtils.getAppSupportDirectoryURL()
     let booksDirectoryPrefix = "books/pdf/"
+    let coverImagesDirectoryPrefix = "books/cover/"
     
     /**
      Fetches an [Array] containing all the [Book]s data.
@@ -80,6 +81,39 @@ class BooksManager {
         bookReference.write(toFile: localURL) { (url, error) in
             if let error = error {
                 print("Could not fetch " + fileName + " : \n" + error.localizedDescription)
+                completion(nil)
+            }
+            
+            if let url = url {
+                print("Download Task success: \(url)")
+                completion(url)
+            }
+        }
+    }
+    
+    /**
+     Downloads the book cover image and returns the local [URL] of the file,
+     or directly returns the [URL] if the cover image has been already downloaded.
+     */
+    func getBookCoverImageLocalURL(imageFileName: String, completion: @escaping (URL?) -> Void) {
+        // Local path of the file
+        guard let localURL = baseLocalURL?
+            .appendingPathComponent(coverImagesDirectoryPrefix)
+            .appendingPathComponent(imageFileName) else {
+                completion(nil)
+                return
+            }
+        
+        // If book was already downloaded, just return its location
+        if DirectoryUtils.isFileExisting(url: localURL) {
+            completion(localURL)
+            return
+        }
+        
+        let coverReference = storageReference.child(booksDirectoryPrefix + imageFileName)
+        coverReference.write(toFile: localURL) { (url, error) in
+            if let error = error {
+                print("Could not fetch " + imageFileName + " : \n" + error.localizedDescription)
                 completion(nil)
             }
             

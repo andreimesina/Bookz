deb0422b6c1af958ae2eddb71c158e423dbf64ad
Andrei Mesina <andreimesina98@gmail.com>
2020-05-13 02:04:54 +0300

    Added NotesManager

--- Bookz/Managers/NotesManager.swift
+++ Bookz/Managers/NotesManager.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+/**
+ This component is responsible for handling
+ the data layer corresponding to the [Note] model.
+*/
+class NotesManager {
+    
+    let firestore = Firestore.firestore()
+    
+    /**
+     Fetches an [Array] containing all the [Note]s data.
+     */
+    func getAllNotes(completion: @escaping (Array<Note>?) -> Void) {
+        var notes = Array<Note>()
+        
+        firestore.collection("notes")
+            .getDocuments() { querySnapshot, error in
+                if let error = error {
+                    print("Failed to fetch notes: \(error)")
+                    completion(nil)
+                } else if let querySnapshot = querySnapshot {
+                    for document in querySnapshot.documents {
+                        let result = Result {
+                            try document.data(as: Note.self)
+                        }
+                        
+                        switch result {
+                            case .success(let note):
+                                if let note = note {
+                                    notes.append(note)
+                                } else {
+                                    print("Note does not exist.")
+                                }
+                            case .failure(let error):
+                                print("Error decoding note: \(error)")
+                        }
+                    }
+                    completion(notes)
+                }
+            }
+    }
+    
+}
